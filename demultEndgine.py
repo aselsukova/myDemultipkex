f5417f6fe593ecce595dadee9596f429dc9f7f66
minja <zakazserov@gmail.com>
2020-09-03 19:45:13 +0300

    Initial commit

---
+++ demultEndgine.py
@@ -0,0 +1,50 @@
+import editdistance
+
+class demultiplexer():
+    def __init__(self, base_index_dict, maxdist = 2,
+                 undetermined_name = "Und"):
+        # basedict should look like:
+        # {"AAAAA+AAAAA" : sample_name}
+
+        self.base_index_dict = base_index_dict
+        self.base_index_dict["NNNN"] = self.undetermined_name
+        self.index_dict = base_index_dict
+        self.maxdist = maxdist
+        self.undetermined_name = undetermined_name
+
+    def demultiplex(self, index):
+        try: # if we've already seen this index
+            return self.index_dict[index] # return sample name
+        except KeyError:
+            self.index_dict[index] = self.find_index(index)
+        return self.index_dict[index]
+
+    def find_index(self,index):
+        # search for most suitable sample for index
+        # return sample name or 'undetermined'
+
+        results = []
+        # go over samples indexes
+        for i in self.base_index_dict:
+            # compute edit distance for each
+            d = editdistance.eval(i,index)
+            if d == 0: # exact match
+                return i # return and exit
+            if d > self.maxdist: # too bad match, do not save
+                continue
+            results.append((d,i)) # save match; find the best one later
+
+        if len(results)>1:
+            results.sort(key=lambda x:x[0])
+
+            # index matches 2 samples with same edit distance
+            # i.e. there is no unambigious best match
+            if results[0][0]==results[1][0]:
+                return self.undetermined_name
+            else: # there is the best match
+                return results[0][1]
+        elif len(results)==0: # no match found
+            return undetermined_name
+
+    def get_output_names(self):
+        return self.base_index_dict.values()
---
+++ demultiplex.py
@@ -0,0 +1,46 @@
+import argparse
+import gzip
+import os
+from demultEndgine import demultiplexer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fname",type=str)
+parser.add_argument("indexesfile",type=str)
+parser.add_argument("out_dir",type=str)
+
+args = parser.parse_args()
+
+base_index_dict = {}
+with open(args.indexesfile) as fin:
+    for line in fin:
+        k,v = fin.readline().strip().split()
+        base_index_dict[k] = v
+
+if not args.fname.endswith("_1.fq.gz"):
+    raise
+
+r2 = args.fname[:-7]+"2.fq.gz"
+r1 = args.fname
+
+r1 = gzip.open(r1)
+r2 = gzip.open(r2)
+
+out_descriptors = []
+
+DM = demultiplexer()
+
+for r in [0,1]:
+    out_descriptors[r] = dict([(k,gzip.open(
+        os.path.join(args.outdir,k+"_"+str(r+1)+".fq.gz"),"w"))\
+                                for k in DM.get_output_names()])
+
+while r1:
+        rname = r1.readline()
+        index = rname.decode("utf-8").strip().split(":")[-1]
+        sample_name = DM.demultiplex(index)
+        out_descriptors[0][sample_name].write(rname)
+        for i in [1,2,3]:
+            out_descriptors[0][sample_name].write(r1.readline())
+        for i in [0,1,2,3]:
+            out_descriptors[1][sample_name].write(r2.readline())
+
