1247d8ce744afcad7a0fb4134c06cb02ca7e82a6
minja <minja-f@ya.ru>
2020-09-04 15:57:44 +0700

    v0

--- demultEndgine.py
+++ demultEndgine.py
@@ -8,16 +8,17 @@ class demultiplexer():
 
         self.base_index_dict = base_index_dict
         self.undetermined_name = undetermined_name
-        self.base_index_dict["NNNN"] = self.undetermined_name
+        self.base_index_dict[self.undetermined_name] = self.undetermined_name
         self.index_dict = base_index_dict
         self.maxdist = maxdist
 
     def demultiplex(self, index):
         try: # if we've already seen this index
-            return self.index_dict[index] # return sample name
+            return self.index_dict[index]
         except KeyError:
-            self.index_dict[index] = self.find_index(index)
-        return self.index_dict[index]
+            relevant_base_index = self.find_index(index)
+            self.index_dict[index] = self.index_dict[relevant_base_index] # save index for next search
+            return self.index_dict[relevant_base_index]
 
     def find_index(self,index):
         # search for most suitable sample for index
@@ -34,7 +35,9 @@ class demultiplexer():
                 continue
             results.append((d,i)) # save match; find the best one later
 
-        if len(results)>1:
+        if len(results)==1:
+            return results[0][1]
+        elif len(results)>1:
             results.sort(key=lambda x:x[0])
 
             # index matches 2 samples with same edit distance
@@ -44,7 +47,7 @@ class demultiplexer():
             else: # there is the best match
                 return results[0][1]
         elif len(results)==0: # no match found
-            return undetermined_name
+            return self.undetermined_name
 
     def get_output_names(self):
         return self.base_index_dict.values()
--- demultiplex.py
+++ demultiplex.py
@@ -1,12 +1,14 @@
 import argparse
 import gzip
 import os
+import datetime
+import pandas as pd
 from demultEndgine import demultiplexer
 
 parser = argparse.ArgumentParser()
-parser.add_argument("fname",type=str)
-parser.add_argument("indexesfile",type=str)
-parser.add_argument("out_dir",type=str)
+parser.add_argument("input_files",type=str, nargs='+')
+parser.add_argument("--indexesfile",type=str)
+parser.add_argument("--out_dir",type=str)
 
 args = parser.parse_args()
 
@@ -16,34 +18,53 @@ if not os.path.exists(args.out_dir):
 base_index_dict = {}
 with open(args.indexesfile) as fin:
     for line in fin:
-        k,v = fin.readline().strip().split()
+        k,v = line.strip().split()
         base_index_dict[k] = v
 
-if not args.fname.endswith("_1.fq.gz"):
-    raise
-
-r2 = args.fname[:-7]+"2.fq.gz"
-r1 = args.fname
-
-r1 = gzip.open(r1)
-r2 = gzip.open(r2)
-
-out_descriptors = []
+for fname in args.input_files:
+    if not fname.endswith("_1.fq.gz"):
+        raise
 
 DM = demultiplexer(base_index_dict=base_index_dict)
+out_names = DM.get_output_names()
+inp_files = args.input_files
+matrix = pd.DataFrame(0,columns=out_names, index=inp_files)
+out_descriptors = []
+
 
 for r in [0,1]:
-    out_descriptors.append(dict([(k,gzip.open(
-        os.path.join(args.out_dir,k+"_"+str(r+1)+".fq.gz"),"w"))\
-                                for k in DM.get_output_names()]))
+        out_descriptors.append(dict([(k,open(
+            os.path.join(args.out_dir,k+"_"+str(r+1)+".fq.gz"),"w"))\
+                                    for k in DM.get_output_names()]))
 
-while r1:
-        rname = r1.readline()
-        index = rname.decode("utf-8").strip().split(":")[-1]
-        sample_name = DM.demultiplex(index)
-        out_descriptors[0][sample_name].write(rname)
-        for i in [1,2,3]:
-            out_descriptors[0][sample_name].write(r1.readline())
-        for i in [0,1,2,3]:
-            out_descriptors[1][sample_name].write(r2.readline())
+start_time = datetime.datetime.now()
 
+for fname in args.input_files:
+    print ("Processing file",fname)
+    r2 = fname[:-7]+"2.fq.gz"
+    r1 = fname
+
+    r1 = gzip.GzipFile(r1)
+    r2 = gzip.GzipFile(r2)
+
+    count = 0
+    report_count = 500000
+    for line in r1:
+            count += 1
+            rname = line
+            decoded = rname.decode("utf-8")
+            index = decoded.strip().split(":")[-1]
+            sample_name = DM.demultiplex(index)
+            matrix.loc[fname,sample_name] += 1
+            out_descriptors[0][sample_name].write(decoded)
+            for i in [1,2,3]:
+                out_descriptors[0][sample_name].write(r1.readline().decode("utf-8"))
+            for i in [0,1,2,3]:
+                out_descriptors[1][sample_name].write(r2.readline().decode("utf-8"))
+            
+            if count > report_count and count % report_count == 0:
+                    timediff = (datetime.datetime.now() - start_time).total_seconds()
+                    print ("Processed: ",count," time: ",timediff," sec")
+print ("Processed: ",count," time: ",timediff," sec")
+matrix.to_csv(os.path.join(args.out_dir,"stats.csv"))
+print ("Done!")
